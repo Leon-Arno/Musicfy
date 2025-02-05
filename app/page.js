82058bca1fd0c214d74bbbc6768d7826609454eb
Leon-Arno <8118254+Leon-Arno@users.noreply.github.com>
2025-02-05 15:13:25 +0100

    moved header/navbar to layout

--- app/page.js
+++ app/page.js
@@ -4,43 +4,6 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <>
-      <header>
-        <div className='container'>
-          <nav className='flex items-center justify-between py-4 flex-wrap'>
-            <div>
-              <span className='font-medium text-xl'>Musicfy</span>
-            </div>
-
-            <ul className='flex font-regular text-sm space-x-8'>
-              <li>
-                <Link href='/trending'>Trending</Link>
-              </li>
-              <li>
-                <Link href='/artists'>Artists</Link>
-              </li>
-              <li>
-                <Link href='/pricing'>Pricing</Link>
-              </li>
-              <li>
-                <Link href='/about-us'>About us</Link>
-              </li>
-            </ul>
-
-            <div className='flex items-center'>
-              <Link className='text-sm mr-8' href=''>
-                Log in
-              </Link>
-              <Link
-                className='font-medium text-sm bg-black text-white px-5 py-2 rounded-sm'
-                href=''
-              >
-                Get started
-              </Link>
-            </div>
-          </nav>
-        </div>
-      </header>
-
       <main>
         <section>
           <div className='container'>
@@ -55,7 +18,7 @@ export default function Home() {
                 industry.
               </p>
               <Link
-                className='inline-block py-3 px-5 bg-black text-white rounded-sm'
+                className='inline-block py-3 px-5 bg-black text-white rounded-sm font-medium'
                 href={''}
               >
                 Get Started for Free
